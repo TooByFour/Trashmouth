45ccf43aa62753cf899b9a92db15d0212ee5c3e2
Tobias Heinrich <t.heinrich@stud.unibas.ch>
2026-09-11 15:37:21 +0200

    making sandbox main

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 #include <iostream>
 
 int main() {
     std::cout << "this is a string" << std::endl;
 }
+=======
+#include <stdio>
+
+int main() { std::cout << "this is a string" << std::endl; }
+>>>>>>> 7327306 (making sandbox main)
